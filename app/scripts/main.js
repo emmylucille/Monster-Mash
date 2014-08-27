08da9e9c03a0bf41f0a26a6396f4610e910cfbab
Emily Bond <emilymbond@yahoo.com>
2014-08-27 16:38:17 -0400

    worked on gulp errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,8 @@
 // e.g. function Monster(){};
 // e.g. function Player(){};
 
+"use strict";
+
 function Player(name){
     this.name = name;
 }
@@ -20,21 +22,21 @@ function Monster(name, health){
 
 var nun  = new Player ({
   name: "Nun"
-})
+});
 
 var priest  = new Player ({
   name: "Priest"
-})
+});
 
 var godzilla = new Monster ({
   name: "Godzilla",
   health: 0
-})
+});
 
 var yeti = new Monster ({
   name: "Yeti",
   health: 0
-})
+});
 
 
 // ACTIONS
@@ -47,23 +49,23 @@ var yeti = new Monster ({
 //   Monster.health -5
 // }
 
-$("#nunFight").hide()
-$("#priestFight").hide()
-$(".pow").hide()
-$(".wham").hide()
-$(".argh").hide()
+$("#nunFight").hide();
+$("#priestFight").hide();
+$(".pow").hide();
+$(".wham").hide();
+$(".argh").hide();
 
 $(".playerNun" ).click(function() {
   $("#nunFight").show("slow");
   $("#start").hide();
-  alert("Are you ready to whip him into shape!")
+  // alert("Are you ready to whip him into shape!");
   $(".monster").css("opacity", 1);
 });
 
 $(".playerPriest" ).click(function() {
   $("#priestFight").show("slow");
   $("#start").hide();
-  alert("Good will prevail!")
+  // alert("Good will prevail!");
   $(".monster").css("opacity", 1);
 });
 
@@ -148,6 +150,6 @@ $("li").click(function() {
     $("#start").show("slow");
     $("#nunFight").hide();
     $("#priestFight").hide();
-    alert("You win! Let's play again");
+    // alert("You win! Let's play again");
   }
 });
